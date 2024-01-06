f0bbcfad1ce55312fc8c2c7a3849b01122a31604
Ishwar <ishwar2532000@gmail.com>
2024-01-06 19:57:28 +0530

    add class

--- src/test/java/test/ProductDescriptionTest.java
+++ src/test/java/test/ProductDescriptionTest.java
@@ -1,19 +1,24 @@
 package test;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 
 import pojo.LaunchBrowser;
 import pom.AddProductfromDescriptionPage;
 import pom.NaaptolHomePage;
 import pom.ProductResultPage;
 import utility.Reports;
-
+@Listeners(test.Listener.class)
 public class ProductDescriptionTest extends BaseTest {
 	
 	ExtentReports extentReport;
@@ -41,9 +46,31 @@ public class ProductDescriptionTest extends BaseTest {
 		
 		AddProductfromDescriptionPage addToCartDescriptionPage=new AddProductfromDescriptionPage(driver);
 		addToCartDescriptionPage.switchToPage(driver, "https://www.naaptol.com/smart-watches/bluetooth-calling-smart-watch-with-neckband-and-mobile-stand-sc6/p/12612081.html");
-		addToCartDescriptionPage.clikOnBuyButton();
+		addToCartDescriptionPage.clikOnBuyButton();		
+   }
+   
+	@AfterMethod
+	public void addTestStatus(ITestResult result) {
 		
-		
-		
-}
+		if(result.getStatus()==ITestResult.SUCCESS) {
+			
+			Test.log(Status.PASS, result.getName());
+		}
+		else if(result.getStatus()==ITestResult.FAILURE) {
+			
+			Test.log(Status.FAIL, result.getName());
+		}
+		else if(result.getStatus()==ITestResult.SKIP) {
+			
+			Test.log(Status.SKIP, result.getName());
+		}
+   
+	}
+	
+	 @AfterTest
+    public void publishReports() {
+
+   	 extentReport.flush();
+       }
+	
 }
